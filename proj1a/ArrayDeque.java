ffabcadeba470aee6f2fb2b8955d8e392edbb49a
Caterpillar <ziweiwang98@outlook.com>
2020-10-01 00:24:46 +0800

    Complete part of the ArrayDeque.java, including addFirst(T item), addLast(T item), constructor, private helper method resize(int capacity), isEmpty(), size(), printDeque(),

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,35 +1,102 @@
 public class ArrayDeque<T> {
+    private T[] arr;
+    private int size;
+
+    private double R;
+    // the empty box right before the first item
+    private int front;
+    // the empty box right after the last item
+    private int end;
+
+    /** Creates an empty array deque. */
+    public ArrayDeque() {
+        // The instructions said that "The starting size of your array should be 8.".
+        arr = (T[]) new Object[8];
+        end = 1;
+    }
+
+    private void resize(int capacity) {
+        T[] newArr = (T[]) new Object[capacity];
+        if (front < end) {
+            System.arraycopy(arr,front + 1, newArr, 0, arr.length);
+            arr = newArr;
+            front = arr.length - 1;
+            end = size;
+        } else {
+            System.arraycopy(arr, front + 1, newArr, 0, arr.length - front - 1);
+            System.arraycopy(arr, 0, newArr, arr.length - front - 1, end);
+            arr = newArr;
+            front = arr.length - 1;
+            end = size;
+        }
+
+    }
+
     /**
      * Adds an item of type T to the front of the deque.
      * */
     public void addFirst(T item) {
-
+        arr[front] = item;
+        // treat array as circular
+        if (front == 0) {
+            front = arr.length - 1;
+        } else {
+            front--;
+        }
+        size++;
+        // resize
+        if (size > arr.length / 2) {
+            resize(arr.length * 2);
+        }
     }
 
     /**
      * Adds an item of type T to the back of the deque.
      * */
     public void addLast(T item) {
-
+        arr[end] = item;
+        // treat array as circular
+        if (end == arr.length - 1) {
+            end = 0;
+        } else {
+            end++;
+        }
+        size++;
+        // resize
+        if (size > arr.length / 2) {
+            resize(arr.length * 2);
+        }
     }
 
     /**
      * Returns true if deque is empty, false otherwise.
      * */
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     /**
      * Returns the number of items in the deque.
      * */
     public int size() {
-        return 0;
+        return size;
     }
 
     /** Prints the items in the deque from first to last, separated by a space. */
     public void printDeque() {
-
+        if (front < end) { // treat arr as a normal array
+            for (int i = front + 1; i < end; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        } else { // arr is circular
+            for (int i = front + 1; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            for (int i = 0; i < end; i++) {
+                System.out.print(arr[i] + " ");
+            }
+        }
     }
 
     /**
@@ -54,5 +121,16 @@ public class ArrayDeque<T> {
      * */
     public T get(int index) {
         return null;
+    }
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> intDeque = new ArrayDeque<>();
+
+        for (int i = 8; i >= 0; i--) {
+            intDeque.addFirst(i);
+        }
+        for (int i = 9; i < 18; i++) {
+            intDeque.addLast(i);
+        }
     }
 }
