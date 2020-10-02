f41523188c94ea5bb3fa149ad1309a3afa227351
Caterpillar <ziweiwang98@outlook.com>
2020-10-02 18:58:24 +0800

    Complete lab2.

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,12 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) {
+            return B;
+        }
+        if (B == null) {
+            return A;
+        }
         IntList p = A;
         while (p.rest != null) {
             p = p.rest;
@@ -96,6 +102,12 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null) {
+            return B;
+        }
+        if (B == null) {
+            return A;
+        }
         IntList result = new IntList();
         IntList res = result;
         result.first = A.first;
