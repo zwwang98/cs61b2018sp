2af36202cf8c6420d1703f48a1f1066320ac44d5
Caterpillar <ziweiwang98@outlook.com>
2020-10-02 18:21:05 +0800

    Complete DebugExercise2.

--- lab2/DebugPractice/DebugExercise2.java
+++ lab2/DebugPractice/DebugExercise2.java
@@ -1,30 +1,47 @@
 /**
  * Exercise to showcase the step over button.
  * Code adapted from https://stackoverflow.com/questions/4895173/bitwise-multiply-and-add-in-java and https://stackoverflow.com/questions/1533131/what-useful-bitwise-operator-code-tricks-should-a-developer-know-about
+ *
+ * The first bug was the method max(int a, int b), it should return the larger one however it actually returns the smaller one.
+ * The second bug was the method add(int a, int b), it just doesn't work the way I think it should be.
+ * And the instructions says that "If you find that one of these functions has a bug, you should completely rewrite it rather than trying to fix it."
  */
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. 
       * This function may have a bug, but if it does, you should find it
       * by stepping over, not into. */
     public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+    /* The code below is the skeleton's version
+    public static int max(int a, int b) {
         int w = (b - a) >> 31;
         /* If you're stepping into this function, click the
            step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
+        /*int z = ~(b - a) >> 31;
 
         int max = b & w | a & z;
         return max;
-    }
+    }*/
+
 
 
     /** Returns the sum of a and b. Do not step into this function. 
       * This function may have a bug, but if it does, you should find it
-      * by stepping over, not into. */    
+      * by stepping over, not into. */
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    /* The code below is the skeleton's version.
     public static int add(int a, int b) {
         int x = a, y = b;
         /* If you're stepping into this function, click the
            step out button because you're not going to learn anything. */
-        int xor, and, temp;
+        /*int xor, and, temp;
         and = x & y;
         xor = x ^ y;
 
@@ -35,7 +52,7 @@ public class DebugExercise2 {
             xor = temp;
         }
         return xor;
-    }
+    }*/
 
     /** Returns a new array where entry i is the max of
      * a[i] and b[i]. For example, if a = {1, -10, 3}
@@ -60,7 +77,9 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            // sum = sum + add(sum, x[i]); this is the skeleton's version, obviously it's a bug
+            sum = add(sum, x[i]);
+            // or we could simply write like: sum += x[i]
             i = i + 1;
         }
         return sum;
@@ -80,6 +99,7 @@ public class DebugExercise2 {
     public static void main(String[] args) {
         int[] a = {1, 11, -1, -11};
         int[] b = {3, -3, 2, -1};
+        // expected result {3, 11, 2, -1}, so the sumOfElementwiseMaxes should be 15
 
         int sumOfElementwiseMaxes = sumOfElementwiseMaxes(a, b);
         System.out.println(sumOfElementwiseMaxes);
